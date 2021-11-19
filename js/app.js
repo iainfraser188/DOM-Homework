bd93ed1cf6c9249b3ef7ad4c803ff77a475a893d
iain <iainfraser188@msn.com>
2021-11-19 16:13:48 +0000

    finnished form html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const  newGame= document.querySelector('#New-Game');
+    newItem.addEventListener('submit', handleNewItemFormSubmit);
+  
+    // const deleteAllButton = document.querySelector('#delete-all');
+    // deleteAllButton.addEventListener('click', handleDeleteAllClick);
+  })
