7fde7b5a1fe59bc1c5799e1d44c6e69af671af44
iain <iainfraser188@msn.com>
2021-11-20 00:27:10 +0000

    adds to list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const  newGame= document.querySelector('#New-Game');
-    newItem.addEventListener('submit', handleNewItemFormSubmit);
+    const  newGame = document.querySelector('#New-Game');
+    newGame.addEventListener('submit', newGameSubmit);
   
-    // const deleteAllButton = document.querySelector('#delete-all');
-    // deleteAllButton.addEventListener('click', handleDeleteAllClick);
-  })
+    const deleteButton = document.createElement('Button');
+    deleteButton.textContent = "Delete all";
+    deleteButton.addEventListener('click', handleDeleteClick)
+    
+    
+});
+
+
+const newGameSubmit = function (event){
+    // console.log(event.target)
+    event.preventDefault();
+    const newGame = createGameListItem(event.target);
+    const gamesList = document.querySelector("#Games-List");
+    gamesList.appendChild(newGame);
+    event.target.reset();
+};
+
+const createGameListItem = function(form){
+    const newGame = document.createElement('li')
+    // console.log(form)
+
+    const Title = document.createElement('h2');
+    // console.log(form.Title.value)
+    Title.textContent = form.Title.value;
+    newGame.appendChild(Title);
+
+    const platform = document.createElement('h3');
+    platform.textContent = form.platform.value;
+    newGame.appendChild(platform);
+
+    const releaseDate = document.createElement('h3');
+    console.log(form.released.value)
+    releaseDate.textContent = form.released.value;
+    newGame.appendChild(releaseDate)
+
+    return newGame;
+    
+};
+const handleDeleteClick = function(event){
+    const gameList = document.querySelector('#GamesList');
+    gameList.innerHTML='';
+}
